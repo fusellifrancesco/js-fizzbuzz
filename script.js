f2462986ec1c0a35773542fc10031d185cfdcf9a
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-11 15:43:14 +0100

    aggiunta istruzione condizionale IF

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-for (let numero = 1; numero <= 100; numero++){
-    
+for (let numero = 1; numero <= 100; numero++) {
+
+    if ((numero % 3) == 0 && (numero % 5) == 0) {
+
+        console.log("fizzbuzz");
+
+    } else if ((numero % 5) == 0) {
+
+        console.log("buzz");
+
+    } else if ((numero % 3) == 0) {
+
+        console.log("fizz")
+
+    } else {
+        console.log(numero);
+    }
 }
