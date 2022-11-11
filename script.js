7c6e78d08e9c68815026558b7ed519bccf25ec66
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-11 15:57:01 +0100

    aggiunte classi CSS  e implementato JS in HTML

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-for (let numero = 1; numero <= 100; numero++) {
+let container = document.querySelector(".my-container");
+
+for (let numero = 1; numero <= 100; numero++ ) {
 
     if ((numero % 3) == 0 && (numero % 5) == 0) {
 
-        console.log("fizzbuzz");
+        container.innerHTML += '<div class="square"><p>FIZZBUZZ</p></div>';
+        console.log("FIZZBUZZ");
 
     } else if ((numero % 5) == 0) {
 
+        container.innerHTML += '<div class="square"><p>BUZZ</p></div>';
         console.log("buzz");
 
     } else if ((numero % 3) == 0) {
 
+        container.innerHTML += '<div class="square"><p>FIZZ</p></div>';
         console.log("fizz")
 
     } else {
+        
+        container.innerHTML += '<div class="square"><p>' + numero + '</p></div>';
         console.log(numero);
+
     }
 }
